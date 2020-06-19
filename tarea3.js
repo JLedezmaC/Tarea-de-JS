ac3632182bcaaefc703878fb8c28ee549b885a08
Ignacio <juanigcl2532@gmail.com>
2020-06-18 21:01:10 -0600

    Agrego tarea de JS

---
+++ tarea3.js
@@ -0,0 +1,42 @@
+
+//Este es el codgio con un solo while y hecho de una manera sencilla para mi // 
+
+let tamano_triangulo = parseFloat(prompt('Ingrese el numero con respecto al tamano del triangulo'));
+let espacio = " ";
+let contador  = 1;
+let asterisco = ' *';
+
+while ( contador <= tamano_triangulo){
+    espacio = espacio + asterisco
+    console.log(espacio)
+    contador++;  
+} 
+
+
+
+//Este es el codigo para imprimir 7 asteriscos de forma vertical // 
+
+/*let asterisco = "*";
+let contador  = 1;
+let pisos = ' ';
+
+while ( contador <= 7){
+    console.log(asterisco); 
+    console.log(pisos);
+contador++;
+}*/
+
+
+
+//Este es el codigo para imprimir una linea con 7 asteriscos// 
+
+/*let tamano_triangulo = parseFloat(prompt('Ingrese el numero con respecto al tamano del triangulo'));
+let espacio = " ";
+let contador  = 1;
+let asterisco = ' *';
+
+while ( contador <= tamano_triangulo){
+    espacio = espacio + asterisco
+    contador++;  
+} console.log(espacio)
+*/
